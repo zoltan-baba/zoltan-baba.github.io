4d24672c3fd55531e2ca8d19a49c46cf837615d2
zoltan-baba <zoltan.baba@bitrise.io>
2021-09-09 15:31:41 +0200

    add custom Javascript

---
+++ docs/js/feedback-panel.js
@@ -0,0 +1,29 @@
+window.onload = function initialConfig() {
+
+    const pageUrl = window.location.href;
+    const homePage = "zoltan-baba.github.io";
+    const jpHomePage = "zoltan_baba.github.io/ja/index-ja.html";
+
+    function homePageConfig() {
+    
+        if (pageUrl !== homePage && pageUrl !== jpHomePage) {
+            document.querySelector(".breadcrumb-container").style.display = "block";
+            document.querySelector(".feedback-panel").style.display = "block";
+        }
+    };
+
+
+    function languageSelector() {
+        let langParent = document.querySelector("div.toolbar-tools");
+
+        if (pageUrl.indexOf("/ja/") > -1) {
+            langParent.querySelectorAll("li")[1].classList.add("active-lang");
+        }
+        else {
+            langParent.querySelectorAll("li")[0].classList.add("active-lang");
+        }
+    };
+
+    homePageConfig();
+    languageSelector();
+};
