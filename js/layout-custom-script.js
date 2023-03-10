59200d4776de0aed2cdc91d2bd52dbe1f727752f
zoltan-baba <zoltan.baba@bitrise.io>
2023-03-10 15:19:27 +0100

    testing intercom

--- js/layout-custom-script.js
+++ js/layout-custom-script.js
@@ -1,114 +1,164 @@
 function initialConfig() {
+  const pageUrl = window.location.href;
+  const homePage = 'https://devcenter.bitrise.io/';
+  const jpHomePage = 'https://devcenter.bitrise.io/ja/index-ja.html';
+  const feedbackPanel = document.querySelector('div.feedback-panel');
 
-    const pageUrl = window.location.href;
-    const homePage = "https://devcenter.bitrise.io/";
-    const jpHomePage = "https://devcenter.bitrise.io/ja/index-ja.html";
+  let kbWrapper = document.createElement('div');
+  let kbCta = document.createElement('a');
 
-    function homePageConfig() {
-    
-        if (pageUrl !== homePage && pageUrl !== jpHomePage) {
-            document.querySelector(".breadcrumb-container").style.display = "block";
-            document.querySelector(".feedback-panel").style.display = "block";
-        }
-    };
+  kbWrapper.setAttribute('class', 'kb-wrapper');
 
-    function advSearchAdd() {
-        let searchBar = document.querySelector(".tool-search");
-        let searchParent = searchBar.parentNode;
-        searchParent.insertBefore(searchBar, searchParent.firstChild.nextSibling);
+  kbCta.setAttribute('class', 'kb-cta');
+  kbCta.setAttribute('href', 'https://support.bitrise.io/hc/en-us/categories/360000108597-Knowledge-Data-Base-?utm_source=devcenter&utm_medium=bottom_cta');
+  kbCta.setAttribute('target', '_blank');
+  kbCta.textContent = 'Visit our Knowledge Base!';
 
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute("class", "adv-search");
-    
-        let newA = document.createElement('a');
-        newA.setAttribute("href", "/search.html");
-        newA.setAttribute("class", "adv-search-link");
-        newA.setAttribute("target", "_blank");
-        newA.textContent = "Advanced search";
-    
-        newDiv.appendChild(newA);
-        searchBar.insertAdjacentElement("afterend", newDiv);
-    };
+  let intCta = kbCta.cloneNode();
+  intCta.setAttribute('class', 'int-cta');
+  intCta.setAttribute('href', 'https://bitrise.io/integrations');
+  intCta.textContent = 'Check out all Bitrise Steps!';
 
-    function langSwitcher() {
+  kbWrapper.appendChild(kbCta);
+  kbWrapper.appendChild(intCta);
 
-        let dropdownContainer = document.createElement('div');
-        dropdownContainer.setAttribute("class", "dropdown-container");
+  feedbackPanel.insertAdjacentElement('beforebegin', kbWrapper);
 
-        let menuText = document.createElement('span');
-        dropdownContainer.appendChild(menuText);
+  function advSearchAdd() {
+    let searchBar = document.querySelector('.tool-search');
+    let searchParent = searchBar.parentNode;
+    searchParent.insertBefore(searchBar, searchParent.firstChild.nextSibling);
 
-        let dropdownContent = document.createElement('ul');
-        dropdownContent.setAttribute("class", "dropdown-content");
-    
-        let langOptionJa = document.createElement('a');
-        langOptionJa.setAttribute("href", "https://devcenter.bitrise.io/ja/index-ja.html");
-        langOptionJa.textContent = "日本語"
-    
-        let langOptionEn = document.createElement('a');
-        langOptionEn.setAttribute("href", "https://devcenter.bitrise.io/");
-        langOptionEn.textContent = "EN"
-    
-        let liItemEn = document.createElement('li');
-        liItemEn.setAttribute("class", "lang-option");
-        let liItemJa = liItemEn.cloneNode();
-    
-        liItemEn.appendChild(langOptionEn);
-        liItemJa.appendChild(langOptionJa);
-    
-        dropdownContent.appendChild(liItemEn);
-        dropdownContent.appendChild(liItemJa);
-        dropdownContainer.appendChild(dropdownContent);
-        document.querySelector(".adv-search").insertAdjacentElement("afterend", dropdownContainer);
-    
-        if (pageUrl.indexOf("/ja/") > -1) {
-            liItemJa.classList.add("active-lang");
-            menuText.textContent = "日本語";
-        }
-        else if (pageUrl.indexOf("/en/") > -1) {
-            liItemEn.classList.add("active-lang");
-            menuText.textContent = "EN";
-            let breadCrumbHome = document.querySelector(".breadcrumb-link");
-            let breadCrumbLink = breadCrumbHome.querySelector("a[href*='index-en.html']");
-            breadCrumbLink.href = "/";
-            breadCrumbLink.textContent = "Home";
-        }
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'adv-search');
 
-        else {
-            liItemEn.classList.add("active-lang");
-            menuText.textContent = "EN";
-        }
-    
-        let jaUrl = pageUrl.replace("/en/", "/ja/");
-        let enUrl = pageUrl.replace("/ja/", "/en/");
-  
-        if (pageUrl !== homePage && pageUrl !== jpHomePage) {
-            langOptionJa.setAttribute("href", jaUrl);
-            langOptionEn.setAttribute("href", enUrl);
-        }
+    let newA = document.createElement('a');
+    newA.setAttribute('href', '/search.html');
+    newA.setAttribute('class', 'adv-search-link');
+    newA.setAttribute('target', '_blank');
+    newA.textContent = 'Advanced search';
 
-        function toggleDropdown() {
-            dropdownContent.classList.toggle("show");
-        }
+    newDiv.appendChild(newA);
+    searchBar.insertAdjacentElement('afterend', newDiv);
+  }
 
-        dropdownContainer.addEventListener("click", toggleDropdown);
-    };
+  function langSwitcher() {
+    let dropdownContainer = document.createElement('div');
+    dropdownContainer.setAttribute('class', 'dropdown-container');
 
+    let menuText = document.createElement('span');
+    dropdownContainer.appendChild(menuText);
 
-    advSearchAdd()
-    homePageConfig();
-    langSwitcher();
+    let dropdownContent = document.createElement('ul');
+    dropdownContent.setAttribute('class', 'dropdown-content');
 
-    /* Adding cookie consent banner scripts */
+    let langOptionJa = document.createElement('a');
+    langOptionJa.setAttribute('href', 'https://devcenter.bitrise.io/ja/index-ja.html');
+    langOptionJa.setAttribute('hreflang', 'ja');
+    langOptionJa.textContent = '日本語';
 
-    let scriptOne = document.createElement('script');
-    scriptOne.src = "https://cdn.cookielaw.org/consent/a8f28a53-d138-4cac-a6dc-bdafd1611beb/OtAutoBlock.js";
-    document.head.appendChild(scriptOne);
-    
-    let scriptTwo = document.createElement('script');      
-    scriptTwo.src = "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"; 
-    scriptTwo.setAttribute("data-domain-script", "a8f28a53-d138-4cac-a6dc-bdafd1611beb");
-    document.head.appendChild(scriptTwo);
-};
+    let langOptionEn = document.createElement('a');
+    langOptionEn.setAttribute('href', 'https://devcenter.bitrise.io/');
+    langOptionEn.setAttribute('hreflang', 'en');
+    langOptionEn.textContent = 'EN';
+
+    let liItemEn = document.createElement('li');
+    liItemEn.setAttribute('class', 'lang-option');
+    let liItemJa = liItemEn.cloneNode();
+
+    liItemEn.appendChild(langOptionEn);
+    liItemJa.appendChild(langOptionJa);
+
+    dropdownContent.appendChild(liItemEn);
+    dropdownContent.appendChild(liItemJa);
+    dropdownContainer.appendChild(dropdownContent);
+    document.querySelector('.adv-search').insertAdjacentElement('afterend', dropdownContainer);
+
+    if (pageUrl.indexOf('/ja/') > -1) {
+      liItemJa.classList.add('active-lang');
+      menuText.textContent = '日本語';
+    } else if (pageUrl.indexOf('/en/') > -1) {
+      liItemEn.classList.add('active-lang');
+      menuText.textContent = 'EN';
+    } else {
+      liItemEn.classList.add('active-lang');
+      menuText.textContent = 'EN';
+    }
+
+    let jaUrl = pageUrl.replace('/en/', '/ja/');
+    let enUrl = pageUrl.replace('/ja/', '/en/');
+
+    if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf('/en/') > -1) {
+      langOptionJa.setAttribute('href', jaUrl);
+      langOptionEn.setAttribute('href', pageUrl);
+      feedbackPanel.style.display = 'flex';
+    } else if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf('/ja/') > -1) {
+      langOptionJa.setAttribute('href', pageUrl);
+      langOptionEn.setAttribute('href', enUrl);
+      feedbackPanel.style.display = 'flex';
+    }
+
+    document.querySelector('.voting-title').textContent = `Was this page helpful?`;
+
+    function toggleDropdown() {
+      dropdownContent.classList.toggle('show');
+    }
+
+    dropdownContainer.addEventListener('click', toggleDropdown);
+  }
+
+  advSearchAdd();
+  langSwitcher();
+
+  /* Adding cookie consent banner scripts */
+
+  let scriptOne = document.createElement('script');
+  scriptOne.src = 'https://cdn.cookielaw.org/consent/74dfda25-8e61-4fab-9330-4718635e7050/OtAutoBlock.js';
+  document.head.appendChild(scriptOne);
+
+  let scriptTwo = document.createElement('script');
+  scriptTwo.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
+  scriptTwo.setAttribute('data-domain-script', '74dfda25-8e61-4fab-9330-4718635e7050');
+  document.head.appendChild(scriptTwo);
+
+}
+
+// GTM
+const gtmContainerId = 'GTM-TZK32GR';
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+// NOTE: Manual OneTrust categorization override to unblock gtm.js loading
+j.setAttribute('class','optanon-category-C0002'); 
+f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',gtmContainerId);
 
 initialConfig();
+
+const tabs = document.querySelectorAll("p.tabs");
+const sections = document.querySelectorAll("div.tab-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", e => {
+    e.preventDefault();
+    removeActiveTab();
+    addActiveTab(tab);
+  });
+})
+
+const removeActiveTab = () => {
+  tabs.forEach(tab => {
+    tab.classList.remove("is-active");
+  });
+  sections.forEach(section => {
+    section.classList.remove("is-active");
+  });
+}
+
+const addActiveTab = tab => {
+  tab.classList.add("is-active");
+  const proc = tab.nextElementSibling.nextElementSibling;
+  console.log(proc);
+  proc.classList.add("is-active");
+}
--- js/script-api.js
+++ js/script-api.js
@@ -1,5 +1,13 @@
 /* JS file for API style output */
 
+/* JS initialize for language dropdown menu */
+var contentBar = $('.nav-code-selection'),
+dropdown = $('.dropdown-content'),
+more = $('a.grouped-link'),
+dropselect = $('.dropselect'),
+selected = '';
+/* End: JS initialize for language dropdown menu */
+
 /**
  * @type {object}
  */
@@ -118,4 +126,116 @@ jQuery(document).ready(function($){
         return e.preventDefault();
     });
     $( "#nav-code-selection a:first" ).click();
+
+
+/* JS for language dropdown menu */
+
+var contentBar = $('.nav-code-selection'),
+dropdown = $('.dropdown-content'),
+more = $('a.grouped-link'),
+dropselect = $('.dropselect'),
+selected = '';
+
+$(window).resize(update);
+
+/*  Opens dropdown menu */
+more.click(function () {
+    dropdown.slideToggle(200);
+});
+
+/*  Closes dropdown menu when click outside */
+$(document).click(function(event) { 
+  var $target = $(event.target);
+  if(!$target.closest('.grouped-link').length && 
+  dropdown.height() > 1) {
+    dropdown.slideUp(200);
+  }        
+});
+
+contentBar.on('click', 'li.item-link', function () {
+    linkClick.call(this, '⋮')
+});
+dropdown.on('click', 'li.item-link', function () {
+    linkClickDropdown.call(this, '⋮')
+});
+update();
+    
+    
+    function linkClick(val) {
+      contentBar.children().removeClass('active');
+      dropdown.children().removeClass('active');
+      this.classList.add("active");
+      selected = this.innerHTML;
+      more.html(val || selected)[val ? 'removeClass' : 'addClass']('active');
+      dropdown.slideUp(200);
+      dropselect.text("");
+      dropselect.css("display","none");      
+    }
+    function linkClickDropdown(val) {
+      contentBar.children().removeClass('active');
+      dropdown.children().removeClass('active');
+      this.classList.add("active");
+      selected = this.innerHTML;
+      more.html(val || selected)[val ? 'removeClass' : 'addClass']('active');
+      dropdown.slideUp(200);
+      var $this = $(this);
+      dropselect.text($this.text());
+      dropselect.css("display","");
+      showcode.call(this);
+    }
+    
+    function update() {
+      var offsetLeft = more.offset().left + 17;
+      var hiddenitems = 0
+      dropdown.empty();
+      contentBar.children().each(function(i, a) {
+      // populates more-dropdown if menu item is overflowed
+      $(a).css("visibility", "visible");
+        var $linkleft = $(a).offset().left;
+        var $linkwidthraw = $(a).css("width");
+        var $linkwidth=$linkwidthraw.replace("px","");
+        var $linkright = +$linkleft + +$linkwidth;
+        if ($linkright > offsetLeft) {
+          if (a.innerHTML == selected) {
+            a.classList.add("active");
+            //more.html(a.innerHTML).addClass('active');
+          }
+          dropdown.append($(a).clone());
+          $(a).css("visibility", "hidden"); // hide menu item if partly overflowed
+          hiddenitems=hiddenitems + 1;
+        } else if (a.innerHTML == more.html()) {
+          more.html('&#8942;').removeClass('active');
+          return false;
+        }
+      })
+      if (hiddenitems > 0){
+        $('.dropdown').css("visibility", "visible"); // Show More dropdown only if there are overflown menu items
+      }
+      else{
+       $('.dropdown').css("visibility", "hidden");
+      }
+    }
+    
+    function showcode(){
+        // Shows selected code. (Copy of $( "#nav-code-selection a" ).click(function(e) but modified for dropdown menu) 
+        var $this = $(this.firstChild);  // this is the clicked li-item, the function uses its child a-element as this
+        var $parent = $this.parent().addClass('active');
+        $this.closest('.nav')
+            .find('.active')
+            .not($parent)
+            .removeClass('active');
+        var language = $this.data( "code" );
+        /*Needs to be explicit to not match regular code elements*/
+        $( ".code[data-code-view]" ).hide();
+        /*$( "*[data-code-view='" + $this.data( "code" ) + "']" ).fadeIn(50);*/
+        var regex = new RegExp("(^|;)" + language + "(;|$)", "g");
+    
+        $('*[data-code-view]')
+        .filter(function() {
+            return $(this).attr('data-code-view').match(regex);
+        })
+        .fadeIn(50);
+        $(window).trigger('scroll');
+    }
+    /* End: JS for language dropdown menu */
 });
